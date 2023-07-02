09ba9a5df50605da6021441fa4bfcf2f409a8d16
AndyWeasley2004 <m5yao@ucsd.edu>
2023-07-02 12:49:48 -0700

    Includes the index page of recipe web

---
+++ src/main/java/guru/springframework/recipeappspring/RecipeAppSpringApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.recipeappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeAppSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/recipeappspring/RecipeAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.recipeappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
