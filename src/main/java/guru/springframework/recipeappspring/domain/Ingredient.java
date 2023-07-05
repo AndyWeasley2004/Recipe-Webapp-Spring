680772b26b6853466078b09cf9ba7448ba98a191
AndyWeasley2004 <m5yao@ucsd.edu>
2023-07-04 21:28:21 -0700

    Fix version bugs that the ID has collision

--- src/main/java/guru/springframework/recipeappspring/domain/Ingredient.java
+++ src/main/java/guru/springframework/recipeappspring/domain/Ingredient.java
@@ -20,10 +20,9 @@ public class Ingredient {
      private UnitOfMeasure uom;
 
     public Ingredient(){}
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
         this.description = description;
         this.amount = amount;
-        this.recipe = recipe;
         this.uom = uom;
     }
     public Long getId() {
--- src/main/java/guru/springframework/recipeappspring/domain/Recipe.java
+++ src/main/java/guru/springframework/recipeappspring/domain/Recipe.java
@@ -38,6 +38,12 @@ public class Recipe {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    public Recipe addIngredient(Ingredient ingredient){
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
     public Long getId() {
         return id;
     }
